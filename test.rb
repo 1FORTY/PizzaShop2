20481ae685bee760fcf61fd9a4a7e206daf567c2
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-10-03 20:22:08 +0700

    Add test file

--- test.rb
+++ test.rb
@@ -1,5 +1,16 @@
+
+hh = {}
 loop do
 
-  
+  print "Enter product id: "
+  id = gets.strip
 
+  print "Enter amount(how much items you want to order): "
+  n = gets.strip.to_i
+
+  x = hh[id].to_i + n
+  hh[id] = x
+  
+  puts hh.inspect
+  puts "======================================"
 end
