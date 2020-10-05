154eee10812a55ce517359fbf417c62f71d89938
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-10-05 21:45:41 +0700

    Update orders button

--- public/app.js
+++ public/app.js
@@ -17,6 +17,9 @@ function add_to_cart(id)
   x = x * 1 + 1; // x += 1
 
   window.localStorage.setItem(key, x); // hh['product_id'] = x - то есть присваиваем значение ключу
+
+  update_orders_input();
+  update_orders_button();
 }
 
 function cart_get_number_of_items()
@@ -38,15 +41,27 @@ function cart_get_number_of_items()
 function cart_get_orders()
 {
   let orders = '';
-  for (let i = 0; i < window.localStorage.length; i++){
-
+  for (let i = 0; i < window.localStorage.length; i++)
+  {
     let key = window.localStorage.key(i); // Получаем ключ
-    let value = window.localStorage.getItem(key); // Получаем значение
+    let value = window.localStorage.getItem(key); // Получаем значения, в Ruby: hh['key'] = x
 
-    if (key.indexOf('product_') == 0) { // Пока перебираем массив, отбираем нужные нам ключи
-      orders += key + '=' value * 1 ', ';
+    if (key.indexOf('product_') == 0) 
+    {
+      orders += key + ' = ' + value * 1 + ', ';
     }
   }
 
   return orders;
+}
+
+function update_orders_input()
+{
+  let orders = cart_get_orders();
+  $('#orders_input').val(orders);
+}
+
+function update_orders_button() {
+  let text = 'CART (' + cart_get_number_of_items() + ')';
+  $('#orders_button').val(text);
 }
